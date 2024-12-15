dcd6813338f80b8495582427e75ba679b89718c0
Akhmad Bakir <akhmgazbak@gmail.com>
2024-12-15 12:07:20 +0300

    Commit 15122024

---
+++ src/main/java/org/example/Automobile.java
@@ -0,0 +1,15 @@
+package org.example;
+
+// данный класс недоступен для изменений
+public abstract class Automobile {
+    protected double speed;
+    protected double maxSpeed;
+    protected double acceleration;
+    protected double brakingSpeed;
+
+    public void accelerate() {
+    }
+
+    public void brake() {
+    }
+}
---
+++ src/main/java/org/example/ModelZ.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.Tezla;
+
+public class ModelZ extends Tezla {
+
+    public ModelZ() {
+        // переопределите необходимые поля
+        speed = 0;
+        maxSpeed = 300;
+        acceleration = 100;
+        brakingSpeed = 100;
+
+
+    }
+}
